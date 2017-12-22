8f68f3c42f2e2c68e80ec424a33bb31dc8053565
M. Fatih CIRIT <mfcirit@gmail.com>
2017-12-22 11:58:40 +0300

    moved model folder and filled tf_classifier.py

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,13 +1,40 @@
 from styx_msgs.msg import TrafficLight
 import numpy as np
-import cv2
+import tensorflow as tf
+import rospy
+
+# Modified code from tensorflow object detection repo
+# https://github.com/tensorflow/models/blob/master/research/object_detection/object_detection_tutorial.ipynb
+
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
+        self.map_class_to_tl = {
+            1: TrafficLight.GREEN,
+            2: TrafficLight.YELLOW,
+            3: TrafficLight.RED,
+            4: TrafficLight.UNKNOWN
+        }
 
-        #self.model = load_model('TRAINED MODEL')
+        # TODO load classifier
+        # Load a (frozen) Tensorflow model into memory.
+        self.detection_graph = tf.Graph()
+        with self.detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile('./trained_models/sim/frozen_inference_graph.pb', 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
 
-        pass
+            self.session = tf.Session(graph=self.detection_graph)
+            # Definite input and output Tensors for detection_graph
+            self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+            # Each box represents a part of the image where a particular object was detected.
+            self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+            # Each score represent how level of confidence for each of the objects.
+            # Score is shown on the result image, together with the class label.
+            self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+            self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+            self.num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -19,25 +46,19 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
+        # TODO implement light color prediction
+        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+        image_np_expanded = np.expand_dims(image, axis=0)
+        # Actual detection.
+        (boxes, scores, classes, num) = self.session.run(
+            [self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections],
+            feed_dict={self.image_tensor: image_np_expanded})
+        classes = np.squeeze(classes)
+        scores = np.squeeze(scores)
+        indice = np.argmax(scores)
 
-        output = image.copy()
-        result = TrafficLight.UNKNOWN
-        light_color = cv2.cvtColor(image, cv2.COLOR_BRG2HSV)
+        if scores[indice] >= 0.5:
+            rospy.loginfo("TL State = ", str(int(classes[indice])))
+            return self.map_class_to_tl[int(classes[indice])]
 
-        lower_light_color = np.array([10,60,50])
-        upper_light_color = np.array([10,255,255])
-        light_color1 = cv2.inRange(light_color, lower_light_color, upper_light_color)
-
-        lower_light_color1 = np.array([170,70,50])
-        upper_light_color1 = np.array([180,255,255])
-        light_color2 = cv2.inRange(light_color, lower_light_color1, upper_light_color1)
-
-        combined_lights = cv2.addWeighted(light_color1, 1.0, light_color2, 1.0, 0.0)
-        blurred_lights = cv2.GaussianBlur(combined_lights,(15,15),0)
-        circular_lights = cv2.HoughCircles(blurred_lights,cv2.HOUGH_GRADIENT,0.5,40, param1=60,param2=30,minRadius=5,maxRadius=200)
-
-        if circular_lights is not None:
-            result = TrafficLight.RED
-
-        return result.output
+        return TrafficLight.UNKNOWN
